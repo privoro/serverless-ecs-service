f76967f4b30599b5634845d06bc18d5297977327
Jeremy Giberson <Jeremy.Giberson@privoro.com>
2019-09-01 09:54:13 -0700

    tweak to support deployment key in config

--- src/config.js
+++ src/config.js
@@ -1,43 +1,3 @@
-let sample = `
-  serverless-ecs-service:
-    cluster: arn:aws:ecs:us-west-2:492058901556:cluster/serverless-plugin-test
-    hostedzone:
-      name: dev.privoro.com.
-      CertificateArn: arn:aws:acm:us-east-1:492058901556:certificate/12963c0c-226a-4b53-b115-3cba97514edb
-    alb:
-      arn: arn:aws:elasticloadbalancing:us-west-2:492058901556:loadbalancer/app/serverless-plugin-test/e3dd81d3e5b6f9bc
-      dns: serverless-plugin-test-1229887500.us-west-2.elb.amazonaws.com
-      listener:
-        arn: arn:aws:elasticloadbalancing:us-west-2:492058901556:listener/app/serverless-plugin-test/e3dd81d3e5b6f9bc/ff00a98c9bee6bd9
-    vpc:
-      id: vpc-837997e5
-      subnets: # PUBLIC
-        - subnet-fbf6a4b2
-        - subnet-0b80bb6c
-      security-groups: # FOR LOAD BALANCER (EG PUBLIC)
-        - sg-12637469
-    cpu: 1024
-    memory: 2048
-    scale: 1
-    ecr:
-      aws-account-id: 492058901556 # default to aws credentials account
-      namespace: dm # default to stage
-    containers:
-      - name: demo
-        path: bar
-        docker-dir: ./ # default to service path
-        port: 3000
-        healthcheck:
-          path: /status # default to /
-          port: 3000 # default to 80
-          codes: '200-299' # default to 200-299
-          protocol: 'HTTP'  
-`;
-
-const throwErr = (msg) => {
-  throw new Error(`Invalid Plugin Config: ${msg}`);
-};
-
 class Config {
   constructor(obj){
     Object.assign(this,obj);
@@ -49,7 +9,9 @@ class Config {
 }
 
 module.exports = {
-  ConfigFactory: (cfg) => {
+  ConfigFactory: (config) => {
+    // backwards compat, merge config.deployment (new structure) to top level (old structure)
+    let cfg = Object.assign({},  config.deployment || {}, config);
     let hz = cfg.HostedZone || {};
     let alb = cfg.Alb || {};
     let vpc = cfg.Vpc || {};
